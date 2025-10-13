a320325a2f67ebf786e2803560f746d789aa2565
Fontomm <siratskyi-v@class.ztk.org.ua>
2025-10-13 06:02:00 +0000

    new file:   output/Douma.cpp 	modified:   output/Pingvin.cpp 	modified:   output/output/Pingvin

--- output/Pingvin.cpp
+++ output/Pingvin.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
-    double A,B,C,D,alpha;
-    cin >> A >> B >> C >> D >> alpha;
-    A=A+D; D=A-D; A=A-D;
-    D=D+C; C=D-C; D=D-C;
-    A=A+B; B=A-B; A=A-B;
+    double A, B, C, D;
 
-    cout << A << " " << B << " " << C << " " << D << endl;
-    cout << alpha*180.0/M_PI;
+    cout << "Введіть чотири числа (A, B, C, D): ";
+    cin >> A >> B >> C >> D;
+
+    A = A + D;
+    D = A - D;
+    A = A - D;
+
+    D = D + C;
+    C = D - C;
+    D = D - C;
+
+    A = A + B;
+    B = A - B;
+    A = A - B;
+
+    cout << "Результат: A=" << A << "  B=" << B << "  C=" << C << "  D=" << D << endl;
+
     return 0;
 }
